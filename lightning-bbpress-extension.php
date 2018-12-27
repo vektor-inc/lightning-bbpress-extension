aed01273d6ba1edf4aba8681656d88893c86c4d0
kurudrive <kurudrive@gmail.com>
2018-12-27 15:07:52 +0900

    1st commit

---
+++ lightning-bbpress-extension.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Plugin Name:     Lightning BBpress Extension
+ * Plugin URI:
+ * Description:
+ * Author:          Vektor,Inc.
+ * Author URI:
+ * Text Domain:     lightning-bbpress-extension
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Lightning_BBpress_Extension
+ */
+
+ /*-------------------------------------------*/
+ /*	デフォルトのパンくず非表示
+ /*-------------------------------------------*/
+add_action( 'wp_head', 'ltg_bbp_css_override', 3 );
+function ltg_bbp_css_override() {
+	$options         = get_option( 'lightning_theme_options' );
+		$dynamic_css = '.bbp-breadcrumb { display:none; }';
+		// delete before after space
+		$dynamic_css = trim( $dynamic_css );
+		// convert tab and br to space
+		$dynamic_css = preg_replace( '/[\n\r\t]/', '', $dynamic_css );
+		// Change multiple spaces to single space
+		$dynamic_css = preg_replace( '/\s(?=\s)/', '', $dynamic_css );
+		wp_add_inline_style( 'lightning-design-style', $dynamic_css );
+}
+
+/*-------------------------------------------*/
+/*	フォーラムのパンくずリスト書き換え
+/*-------------------------------------------*/
+add_filter(
+	'lightning_panListHtml', function( $panListHtml ) {
+		if ( function_exists( 'bbp_get_forum_post_type' ) ) {
+			$postType = lightning_get_post_type();
+			if ( $postType['slug'] == 'topic' ) {
+
+				// Microdata
+				// http://schema.org/BreadcrumbList
+				/*-------------------------------------------*/
+				$microdata_li = ' itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"';
+
+				$before_html = '<!-- [ .breadSection ] -->
+<div class="section breadSection">
+<div class="container">
+<div class="row">
+<ol class="breadcrumb" itemtype="http://schema.org/BreadcrumbList">';
+
+				$after_html = '</ol>
+</div>
+</div>
+</div>
+<!-- [ /.breadSection ] -->';
+
+				$args = array(
+					// HTML
+					'before'         => $before_html,
+					'after'          => $after_html,
+					'sep'            => '',
+					'crumb_before'   => '<li' . $microdata_li . '><span>',
+					'crumb_after'    => '</span></li>',
+					'home_text'      => '<i class="fa fa-home"></i> HOME',
+					'current_before' => '',
+					'current_after'  => '',
+				);
+				$panListHtml = bbp_get_breadcrumb( $args );
+			}
+		}
+		return $panListHtml;
+	}
+);
