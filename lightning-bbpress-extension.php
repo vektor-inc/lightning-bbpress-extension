87cf53994d0e052f9949e44ee6aef0f7064e98f3
kurudrive <kurudrive@gmail.com>
2020-01-09 04:11:27 +0900

    [ Change version ] 0.1.9

--- lightning-bbpress-extension.php
+++ lightning-bbpress-extension.php
@@ -7,7 +7,7 @@
  * Author URI:
  * Text Domain:     lightning-bbpress-extension
  * Domain Path:     /languages
- * Version:         0.1.8
+ * Version:         0.1.9
  *
  * @package         Lightning_BBpress_Extension
  */
